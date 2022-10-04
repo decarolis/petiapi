0417f03abb3aeb486a6de65866411850e83161ca
Andre De Carolis <decarolis.dev@gmail.com>
2022-10-04 21:03:27 +0100

    cors

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://petiapp.netlify.app/');
+  res.setHeader('Access-Control-Allow-Origin', 'https://petiapp.netlify.app');
   res.setHeader(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept',
