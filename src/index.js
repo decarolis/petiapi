f3bc5878090d38ebba4f6c939e027cd1ae15d52d
Andre De Carolis <decarolis.dev@gmail.com>
2022-10-05 13:30:39 +0100

    crossorigin

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://peti.pt');
   res.setHeader(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept',
